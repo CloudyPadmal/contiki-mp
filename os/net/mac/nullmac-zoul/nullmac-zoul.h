6ddd3b6929d159fcd4d5f5a3566be92d7300765a
Padmal <CloudyPadmal@users.noreply.github.com>
2022-01-20 09:58:55 +0100

    feat: added nullmac driver for zoul motes

---
+++ os/net/mac/nullmac-zoul/nullmac-zoul.h
@@ -0,0 +1,12 @@
+#ifndef NULLMAC_ZOUL_H_
+#define NULLMAC_ZOUL_H_
+
+#include "contiki.h"
+#include "net/mac/mac.h"
+#include "net/packetbuf.h"
+#include "net/netstack.h"
+#include "dev/radio.h"
+
+extern const struct mac_driver nullmac_zoul_driver;
+
+#endif /* NULLMAC_ZOUL_H_ */
